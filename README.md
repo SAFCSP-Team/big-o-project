998c9af1f6d0e53894bf47076798530b7a139bac
malekcoders <alomayri970@outlook.com>
2024-02-27 15:32:05 +0300

    Update Project Code and Problem + add code on readme

--- README.md
+++ README.md
@@ -4,12 +4,58 @@
 The objective of this project is to understand the Big O Notation, and define which algorithm is better than the other.
 
 ### Problem
-You have two algorithms with Big O Notation represented as `O(n^2)` and `O(n)`. Which algorithm is better than the other?
+
+You have two algorithms with Big O time complexity represented as **[Insertion Sort](/src/SelectionSort.java)** `O(n^2)` and **[Heapsort](/src/HeapSort.java)** `O(n log(n))`.
+
+**Question:**
+Which algorithm is better in terms of time complexity?
+And explain why?
 
 
 ### Implementation
 Compare the two algorithms result, and define which algorithm is better, and explain why.
 
+```java
+public class Main {
+    public static void main(String[] args) {
 
-> * Create a new issue with title "Answer [your-username]" and write your answers.
+        int[] arrayOne = { 12, 11, 13, 5, 6, 7, 9, 15, 23, 4, 8, 26, 31 };
+        int[] arrayTwo = { 15, 3, 6, 5, 22, 7, 46, 12, 23, 1, 17, 16, 33 };
+
+        HeapSort algorithmOne = new HeapSort();
+
+        // O(n log n)
+        algorithmOne.heapSort(arrayOne);
+
+        System.out.println("Heap Sort");
+        System.out.println("O(n log n)");
+        for (int i = 0; i < arrayOne.length; i++) {
+            System.out.print(arrayOne[i] + " ");
+        }
+
+        SelectionSort algorithmTwo = new SelectionSort();
+        // O(n^2)
+        algorithmTwo.selectionSort(arrayTwo);
+
+        System.out.println("\nSelection Sort");
+        System.out.println("O(n^2)");
+        for (int i = 0; i < arrayTwo.length; i++) {
+            System.out.print(arrayTwo[i] + " ");
+        }
+    }
+}
+```
+**Output**
+```
+Heap Sort
+O(n log n)
+4 5 6 7 8 9 11 12 13 15 23 26 31 
+Selection Sort
+O(n^2)
+1 3 5 6 7 12 15 16 17 22 23 33 46 
+```
+
+<br>
+
+> * **Create a new issue with title "Answer [your-username]" and write your answer.**
 > * Create an issue of your questions, If you face any trouble solving the project.
--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        
-        /* Which algorithm is better */
-        
-    }
 
-    // Algorithm 1 - Quick Sort
-    // Big O Notation: O(n^2)
-    public static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pivotIdx = partition(arr, low, high);
-            quickSort(arr, low, pivotIdx - 1);
-            quickSort(arr, pivotIdx + 1, high);
-        }
-    }
+        int[] arrayOne = { 12, 11, 13, 5, 6, 7, 9, 15, 23, 4, 8, 26, 31 };
+        int[] arrayTwo = { 15, 3, 6, 5, 22, 7, 46, 12, 23, 1, 17, 16, 33 };
 
-    private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-        return i + 1;
-    }
+        HeapSort algorithmOne = new HeapSort();
 
-    // Algorithm 2 - Merge Sort
-    // Big O Notation: O(n log n)
-    public static void mergeSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int mid = (low + high) / 2;
-            mergeSort(arr, low, mid);
-            mergeSort(arr, mid + 1, high);
-            merge(arr, low, mid, high);
-        }
-    }
+        // O(n log n)
+        algorithmOne.heapSort(arrayOne);
 
-    private static void merge(int[] arr, int low, int mid, int high) {
-        int n1 = mid - low + 1;
-        int n2 = high - mid;
-        int[] left = new int[n1];
-        int[] right = new int[n2];
-        for (int i = 0; i < n1; i++) {
-            left[i] = arr[low + i];
+        System.out.println("Heap Sort");
+        System.out.println("O(n log n)");
+        for (int i = 0; i < arrayOne.length; i++) {
+            System.out.print(arrayOne[i] + " ");
         }
-        for (int j = 0; j < n2; j++) {
-            right[j] = arr[mid + 1 + j];
-        }
-        int i = 0, j = 0, k = low;
-        while (i < n1 && j < n2) {
-            if (left[i] <= right[j]) {
-                arr[k] = left[i];
-                i++;
-            } else {
-                arr[k] = right[j];
-                j++;
-            }
-            k++;
-        }
-        while (i < n1) {
-            arr[k] = left[i];
-            i++;
-            k++;
-        }
-        while (j < n2) {
-            arr[k] = right[j];
-            j++;
-            k++;
+
+        SelectionSort algorithmTwo = new SelectionSort();
+        // O(n^2)
+        algorithmTwo.selectionSort(arrayTwo);
+
+        System.out.println("\nSelection Sort");
+        System.out.println("O(n^2)");
+        for (int i = 0; i < arrayTwo.length; i++) {
+            System.out.print(arrayTwo[i] + " ");
         }
     }
 }
